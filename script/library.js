80956620a723821aa890a0bb378cf66109ef480b
g0vind <krishhhello555@gmail.com>
2020-08-20 20:00:05 +0530

    Created decent version of Library, looks Tablish

--- script/library.js
+++ script/library.js
@@ -1,52 +1,136 @@
-let myLibrary = [];
+let myLibrary = [
+    {
+      title: "Harry Potter",
+      author: "Jk rowling",
+      pages: "899",
+      readStatus: "not read yet"
+    },
+    {
+      title: "The Old Man and Sea",
+      author: "Ernst hemmingway",
+      pages: "200",
+      readStatus: "read"
+    },
+    {
+      title: "The Hobbit",
+      author: "Jrr tolkins",
+      pages: "399",
+      readStatus: "read"
+    }
+  ].map((book) => new Book(book));
 
-function Book(title, author, pages, readStatus) {
+const container = document.querySelector(".library-container");
+const addBtn = document.querySelector(".addbook");
+
+function Book({title, author, pages, readStatus}) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
 }
 
-function addBookToLibrary () {
-    let title = prompt("Enter Title");
-    let author = prompt("Enter author");
-    let pages = prompt("Enter pages");
-    let readStatus = prompt("Enter readStatus");
-    let object = new Book(title, author, pages, readStatus);
-    myLibrary.push(object);
-    return myLibrary;
+
+// Add a book ----------------------------------------------------->
+function addBookToLibrary (e) {
+    e.preventDefault();
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("number").value;
+    let readStatus = document.getElementsByClassName("input-checkbox")[0].checked == true? "read" : "not read";
+    if(title=="" || author=="" ||pages==""){
+        alert("Fill all the necessary details!")
+    } else {
+        let object = new Book({title, author, pages, readStatus});
+        myLibrary.push(object);
+        render();
+        document.getElementById("book-form").reset()
+    }
 }
 
-//const addBtn = document.querySelector(".addBtn");
+document.getElementById("submit").addEventListener('click', addBookToLibrary);
 
-//addBtn.addEventListener('click', addBookToLibrary);
 
-myLibrary = [
-    { title: "Harry potter", author: "jk rowling", pages: "899", readStatus: "not read yet" },
-    { title: "the old man and sea", author: "ernst hemmingway", pages: "200", readStatus: "read" },
-    { title: "The Hobbit", author: "jrr tolkins", pages: "399", readStatus: "read" }
-]
-  
-const container = document.querySelector("#lib");
+// Render books on screen ------------------------------------------------->
+function render() {
+    //clear container before render
+    container.innerHTML = '';
 
-function render(s) {
-    const div = document.createElement('div');
-    div.classList.add('library-books');
-    const s1 = document.createElement('div');
-    const s2 = document.createElement('div');
-    const s3 = document.createElement('div');
-    const s4 = document.createElement('div');
-    s1.textContent = s.title;
-    s2.textContent = s.author;
-    s3.textContent = s.pages;
-    s4.textContent = s.readStatus;
-    container.appendChild(div);
-    div.appendChild(s1);
-    div.appendChild(s2);
-    div.appendChild(s3);
-    div.appendChild(s4);
+    for ( let prop in myLibrary) {
+        s = myLibrary[prop];
+
+        const div = document.createElement('div');
+        const Title = document.createElement('div');
+        const Author = document.createElement('div');
+        const Pages = document.createElement('div');
+        const readstat = document.createElement('div');
+        const BookTrash = document.createElement('button');
+
+        div.classList.add('library-books');
+        BookTrash.classList.add('delBtn','icon-trash','del-btn-style','icon-large');
+        readstat.classList.add('readStat');
+        let bookKey = makeid(15);
+        div.dataset.key = bookKey;
+        BookTrash.dataset.key = bookKey;
+        myLibrary[prop].key = bookKey;
+
+        Title.textContent = s.title;
+        Author.textContent = s.author;
+        Pages.textContent = s.pages;
+        readstat.textContent = s.readStatus;
+        
+        div.appendChild(Title);
+        div.appendChild(Author);
+        div.appendChild(Pages);
+        div.appendChild(readstat);
+        div.appendChild(BookTrash);
+        container.appendChild(div);
+       
+
+        BookTrash.addEventListener('click', () => {
+            BookTrash.parentElement.remove();
+            removeBook();
+        })
+        readstat.addEventListener('click', () => {
+            readstat.textContent = readstat.textContent == 'read'? "not read yet": "read";
+        })   
+    }
 }
 
-for(let prop in myLibrary) {
-    render(myLibrary[prop]);
+addBtn.addEventListener('click', () => {
+    openform();
+});
+
+// Remove items from Library -------------------------------------------------->
+function removeBook(key) {
+    myLibrary.splice(
+        myLibrary.findIndex(x => x.key = key), 1)
 }
+
+
+//Generate data-key-------------------------------------------------------------->
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+ 
+ // open form ------------------------------------------------------------->
+ function openform() {
+    if (addBtn.value == "add") {
+        document.querySelector(".container").style.display = "inline-block";
+        addBtn.value = "done";
+        addBtn.innerHTML = " Done?";
+    } else if (addBtn.value == "done") {
+        document.querySelector(".container").style.display = "none";
+        addBtn.value = "add";
+        addBtn.innerHTML = " Add Book";
+    }
+  }
+
+
+//Render all the books form library---------------------------------------------->
+render();
